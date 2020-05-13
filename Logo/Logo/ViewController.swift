e3f97dccd99264cdc2b4e84a3592233ffc26f02f
Akmal Nurmatov <akmalnurmatov1@gmail.com>
2020-05-13 19:47:11 -0400

    logo made

---
+++ Logo/Logo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by Akmal Nurmatov on 5/13/20.
+//  Copyright © 2020 Akmal Nurmatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
